bb2fc3e368c348fe2b1523a9951a3f6e1b78d3c1
unknown <gdutt2304@gmail.com>
2025-11-26 20:59:10 +0530

    DataType

---
+++ JS/code1.js
@@ -0,0 +1,111 @@
+console.log("Hello from js");
+// document.writeln("hey ");
+// String
+// a = "Ganesh Dutt";
+// console.log(a);
+// var let const
+// var a = "JS";
+// console.log(a);
+
+// var a = "javaScript";
+// console.log(a);
+
+// 500 line
+
+// var name1 = "rahul"; //-  99
+// var name2 = "Kartik"; //-- 178
+// console.log(name1);
+// console.log(name2);
+
+// 1-  rahul1 task
+// 2 rahul2
+
+// let value1 = "hey i am let "; // 99
+// let value2 = "value2"; // 178
+
+// console.log(value1);
+
+// var a = 78;
+// a = 98;
+
+// console.log(a);
+
+// let b = 98;
+// b = "Ganesh ";
+// console.log(b);
+
+// const value = "i am const ";
+// value = 873847;
+// console.log(value);
+
+// const value = "i am const ";
+
+// let  ✔️
+// const  ✔️
+
+// var  ✔️
+
+// DataType
+// n - number , null
+// s - String Symbol
+// b - Boolean , BigInt
+// Undefined
+
+// let number = 7.6;
+// let String = "Ganesh Dutt";
+// let isActivate  = true , false
+
+// let number = BigInt(4348343874838467676777676);
+// console.log(number);
+
+// let result;
+// .
+// .
+// .
+// ./
+// result = "fail";
+// console.log(result);
+
+// let output = "";
+
+// let a = 438498;
+// let b = "9898";
+// console.log(typeof a);
+// console.log(typeof b);
+
+// let studentObj = {
+//   // key :value
+//   name: "xyz",
+//   age: 23,
+//   course: "FS",
+
+// };
+
+// console.log(studentObj.name, studentObj.age);
+
+// let studentArray = ["xyz", 4334837, true];
+// index -------------[0     ,1       ,2]
+// console.log(studentArray[2]);
+
+// function
+
+// let a = 7;
+// let b = 8;
+// console.log(a + b);
+
+// let a1 = 12;
+// let b1 = 57;
+// console.log(a1 + b1);
+
+// let a4 = 12;
+// let b5 = 57;
+// console.log(a1 + b1);
+// function fun(a, b) {
+//   console.log("i am function ");
+//   return console.log("sum  :  ", a + b);
+// }
+// fun(120, 67);
+// fun(10, 67);
+// fun(10, 6);
+
+// console.log(Date());

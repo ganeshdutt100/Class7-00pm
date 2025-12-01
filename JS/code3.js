56a22f131dab5024f3dd7446a34bfb816540bfc4
unknown <gdutt2304@gmail.com>
2025-12-01 21:07:19 +0530

    nested and Switch

--- JS/code3.js
+++ JS/code3.js
@@ -1,4 +1,4 @@
-let a = 45;
+// let a = 45;
 // simple if
 // if(condition){
 //     // stm
@@ -10,13 +10,53 @@ let a = 45;
 //   console.log("odd");
 // }
 
-let marks = 50;
-if (marks >= 60) {
-  console.log("First");
-} else if (marks >= 40) {
-  console.log("Second");
-} else if (marks >= 30) {
-  console.log("Third");
-} else {
-  console.log("fail");
+// let marks = 50;
+// if (marks >= 60) {
+//   console.log("First");
+// } else if (marks >= 40) {
+//   console.log("Second");
+// } else if (marks >= 30) {
+//   console.log("Third");
+// } else {
+//   console.log("fail");
+// }
+
+// nested if else
+// let a = -5;
+// let b = 12;
+// if (a > 0) {
+//   console.log("a is positive");
+//   if (b % 2 == 0) {
+//     console.log("even");
+//   } else {
+//     console.log("odd");
+//   }
+// } else {
+//   console.log("a is negative ");
+// }
+let day = 8;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wed");
+    break;
+  case 4:
+    console.log("Thur");
+    break;
+  case 5:
+    console.log("Fri");
+    break;
+  case 6:
+    console.log("Sat");
+    break;
+  case 7:
+    console.log("Sun");
+    break;
+  default:
+    console.log("Error");
 }

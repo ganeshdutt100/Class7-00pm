483a7e506c93bad520bfa95ae924d987c862e5be
unknown <gdutt2304@gmail.com>
2025-12-01 20:48:26 +0530

    else if

---
+++ JS/code3.js
@@ -0,0 +1,22 @@
+let a = 45;
+// simple if
+// if(condition){
+//     // stm
+// }
+
+// if (a % 2 == 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+let marks = 50;
+if (marks >= 60) {
+  console.log("First");
+} else if (marks >= 40) {
+  console.log("Second");
+} else if (marks >= 30) {
+  console.log("Third");
+} else {
+  console.log("fail");
+}

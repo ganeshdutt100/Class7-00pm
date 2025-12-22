c161c68b23910465603b2c8634bdf36423ec908c
unknown <gdutt2304@gmail.com>
2025-12-22 20:30:11 +0530

    iterationMethods

---
+++ JS/array/Iteration_methods.js
@@ -0,0 +1,32 @@
+let arr = [13, 19, 6, 2, 3, 4, 5, 6];
+// let output = arr.every((x) => x % 2 == 0);
+// let output = arr.some((x) => x % 2 == 0);
+// let output = arr.find((x) => x % 2 == 0);
+// let output = arr.findIndex((x) => x % 3 == 0);
+// let output = arr.map((x) => x + x);
+// let output = arr.reduce((acc, num) => acc + num);
+// let output = arr.forEach((arrNum) => console.log(arrNum));
+// console.log(output);
+// [0,13],[1,19],[2,6]
+// for (let entry of arr.entries()) {
+//   console.log(entry);
+// }
+// for (let key of arr.keys()) {
+//   console.log(key);
+// }
+// for (let value of arr.values()) {
+//   console.log(value);
+// }
+// console.log(output);
+
+// function fun(a) {
+//   return x % 2 == 0;
+// }
+
+// console.log(fun(12));
+
+// let sum = 0;
+// for (let num of arr) {
+//   sum += num;
+// }
+// console.log(sum);

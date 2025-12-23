76d75795215c751b35cf40de78a92a626e1e698b
unknown <gdutt2304@gmail.com>
2025-12-23 20:11:34 +0530

    String

---
+++ JS/String.js
@@ -0,0 +1,25 @@
+let a = " Hello, javaScript , Python , hello , Hello";
+// let b = "Hello, javaScript , Python";
+// let output = a.length;
+// let output = a[0];
+// let output = a.substring(0, 5);
+// let output = a.substr(0, 5);
+// let output = a.split(",");
+// let output = a.concat(b);
+// let output = a.toUpperCase();
+// let output = a.trim();
+// let output = a.indexOf("j");
+// let output = a.lastIndexOf("a");
+// let output = a.replace("Hello", "Hey");
+// let output = a.replace(/Hello/g, "Hey");
+let output = a.replace(/Hello/gi, "Hey");
+console.log(output);
+
+// console.log("Hey " + a + " welcome to the JS world");
+// console.log(`hey ${a} welcome to the js world`);
+// console.log("hello
+//     world ");
+// console.log(`
+//     hello
+//     world
+//     `);

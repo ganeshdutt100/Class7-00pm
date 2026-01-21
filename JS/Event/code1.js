42a5e1f5e9cebf22b91e38f6bd67a819245bf9f0
unknown <gdutt2304@gmail.com>
2026-01-21 19:54:21 +0530

    event

---
+++ JS/Event/code1.js
@@ -0,0 +1,20 @@
+// mouse
+// click
+// dblclick
+// mouseenter
+// mouseleave
+// keyBoard
+// keydown;
+// keyup;
+// keypress
+
+// form
+// submit
+// change
+// focus
+// blur
+
+// window
+// scroll;
+// load;
+// resize;

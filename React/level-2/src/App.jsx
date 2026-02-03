a7d01c32cf7e108a4b34e990b86af5b96146669c
unknown <gdutt2304@gmail.com>
2026-02-03 19:56:13 +0530

    Props

--- React/level-2/src/App.jsx
+++ React/level-2/src/App.jsx
@@ -1,31 +1,30 @@
-
 // import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
 
-import About from './pages/About'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Footer from "./components/layout/Footer";
 
 function App() {
-
-
   return (
     <>
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
-// BrowserRouter 
+export default App;
+// BrowserRouter
 // Routes
 // Route
